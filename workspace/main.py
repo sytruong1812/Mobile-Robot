a4331e5b7de0d2ff6883c4b26977a3a0e573435a
SY TRUONG <sytruong1812@gmail.com>
2023-01-01 09:38:26 +0700

    update

--- workspace/main.py
+++ workspace/main.py
@@ -1,4 +1,5 @@
 import time
+import math
 import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP0, eQEP2
@@ -82,23 +83,6 @@ def left():
 
 # Xe quay ngược 1 góc 180 độ
 
-"""
-    Đường kính bánh xe: Dn = 95mm
-    pi = 3.14
-    Chu vi bánh xe: Dn*pi=95*3.14=298.3 (mm)
-    Độ phân giải encoder 2400 xung/vòng
-    Số mm/1xung=298.3/2400=0,1242916666666667 (mm)
-"""
-def Encoder_Wheel():
-    # Nhận vị trí hiện tại
-    positionA = myEncoderA.position
-    positionB = myEncoderB.position
-    # Pulse -> Distance
-    wheel_A = positionA*(198.3/2400)
-    print ("Distance_Wheel_A: ",wheel_A,"mm")
-    wheel_B = positionB*(298.3/2400)
-    print ("Distance_Wheel_B: ",wheel_B,"mm")
-
 def distanceMeasurement(TRIG,ECHO):
     GPIO.output(TRIG, True)     # set Trigger to HIGH
     time.sleep(0.00001)         # set Trigger after 0.01ms to LOW
@@ -116,6 +100,37 @@ def distanceMeasurement(TRIG,ECHO):
     distance = pulseDuration * 17150        # multiply with the sonic speed (34300 cm/s)
     distance = round(distance, 2)   # Làm tròn
     return distance
+
+"""
+    Đường kính bánh xe: Dn = 95mm
+    pi = 3.14
+    Chu vi bánh xe: Dn*pi=95*3.14=298.3 (mm)
+    Độ phân giải encoder 2400 xung/vòng
+    Số mm/1xung=298.3/2400=0,1242916666666667 (mm)
+"""
+def Encoder_Wheel():
+    # Nhận vị trí hiện tại
+    positionA = myEncoderA.position
+    positionB = myEncoderB.position
+    # Pulse -> Distance
+    wheel_A = positionA*(198.3/2400)
+    print ("Distance_Wheel_A: ",wheel_A,"mm")
+    wheel_B = positionB*(298.3/2400)
+    print ("Distance_Wheel_B: ",wheel_B,"mm")
+
+    # Tính toán vị trí
+    angle_old = 0
+    x_old = 0
+    y_old = 0
+    b = 30  # Khoảng cánh giữa 2 bánh xe
+    midpoint = (wheel_A + wheel_B) / 2        # Độ dịch chuyển tương đối của điểm trung tâm
+    angle_new = (wheel_A + wheel_B) / b         # Góc xoay của xe
+
+    angle_i = angle_old + angle_new       # Hướng tương đối của xe tại thời điểm i
+
+    x_i = x_old + midpoint * math.cos(angle_i)          # Vị trí tương đối trục x của xe tại thời điểm i
+    y_i = y_old + midpoint * math.sin(angle_i)         # Vị trí tương đối trục y của xe tại thời điểm i
+    print ("x: %f,y: %f, angle: %f" % x_i, y_i, angle_i)
 
 while True:
     control = input("Control DC Motor: ")
