94b0ddbd86f2d0adf980e1649d827f6664e859a9
SY TRUONG <sytruong1812@gmail.com>
2023-02-19 21:57:03 +0700

    Update

--- workspace/main.py
+++ workspace/main.py
@@ -26,11 +26,14 @@ PWM.start(PWMA, 100, 2000, 1)
 PWM.start(PWMB, 100, 2000, 1)
 
 # Khởi tạo lớp để truy cập kênh eQEP1, eQEP2 và chỉ khởi tạo kênh đó
-myEncoderA = RotaryEncoder(eQEP0)       #eQEP0    P9.27    P9.42
-myEncoderB = RotaryEncoder(eQEP2)       #eQEP2    P8.11    P8.12
+myEncoderA = RotaryEncoder(eQEP2)       #eQEP0    P9.27    P9.42
+myEncoderB = RotaryEncoder(eQEP0)       #eQEP2    P8.11    P8.12
 # Chế độ tuyệt đối: vị trí bắt đầu từ 0 và được tăng hoặc giảm theo chuyển động của bộ mã hóa
 myEncoderA.setAbsolute()
 myEncoderB.setAbsolute()
+
+wheel_A = 0
+wheel_B = 0
 
 """Ultrasonic Sensor"""
 GPIO.setup("P9_15",GPIO.OUT) #Trigger
@@ -40,22 +43,14 @@ GPIO.setup("P9_12",GPIO.IN)  #Echo
 GPIO.output("P9_15", False)
 time.sleep(0.5)
 
-
-def Stopmotor():
-    GPIO.output(IN1,GPIO.LOW)
-    GPIO.output(IN2,GPIO.LOW)
-    GPIO.output(IN3,GPIO.LOW)
-    GPIO.output(IN4,GPIO.LOW)
-    PWM.set_duty_cycle(PWMA, 0)
-    PWM.set_duty_cycle(PWMB, 0)
-def Backward():
+def Forward():
     GPIO.output(IN1,GPIO.HIGH)
     GPIO.output(IN2,GPIO.LOW)
     GPIO.output(IN3,GPIO.HIGH)
     GPIO.output(IN4,GPIO.LOW)
     PWM.set_duty_cycle(PWMA, 25)
     PWM.set_duty_cycle(PWMB, 25)
-def Forward():
+def Backward():
     GPIO.output(IN1,GPIO.LOW)
     GPIO.output(IN2,GPIO.HIGH)
     GPIO.output(IN3,GPIO.LOW)
@@ -74,6 +69,13 @@ def Left():
     GPIO.output(IN3,GPIO.HIGH)
     GPIO.output(IN4,GPIO.LOW)
     PWM.set_duty_cycle(PWMA, 25)
+def Stopmotor():
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.LOW)
+    PWM.set_duty_cycle(PWMA, 0)
+    PWM.set_duty_cycle(PWMB, 0)
     
 # Xe quay 1 góc 90 độ
 
@@ -97,7 +99,7 @@ def DistanceMeasurement(TRIG,ECHO):
         pulseEnd = time.time()
 
     pulseDuration = pulseEnd - pulseStart
-    distance = pulseDuration * 17150        # multiply with the sonic speed (34300 cm/s)
+    distance = pulseDuration * 17150        # multiply with the sonic speed (34250 cm/s)
     distance = round(distance, 2)   # Làm tròn
     return distance
 
@@ -108,7 +110,7 @@ def DistanceMeasurement(TRIG,ECHO):
     Độ phân giải encoder 2400 xung/vòng
     Số mm/1xung=298.3/2400=0,1242916666666667 (mm)
 """
-def Encoder_Wheel():
+def Position(x_old, y_old, angle_old):
     # Nhận vị trí hiện tại
     positionA = myEncoderA.position
     positionB = myEncoderB.position
@@ -118,11 +120,8 @@ def Encoder_Wheel():
     wheel_B = positionB*(298.3/2400)
     print ("Distance_Wheel_B: ",wheel_B,"mm")
 
-def Position(x_old, y_old, angle_old):
-    b = 30  # Khoảng cánh giữa 2 bánh xe
-    global wheel_A
-    global wheel_B
-    
+    b = 25  # Khoảng cánh giữa 2 bánh xe
+
     midpoint = (wheel_A + wheel_B) / 2        # Độ dịch chuyển tương đối của điểm trung tâm
     angle_new = (wheel_A + wheel_B) / b         # Góc xoay của xe
 
@@ -131,19 +130,20 @@ def Position(x_old, y_old, angle_old):
     x_i = x_old + midpoint * math.cos(angle_i)          # Vị trí tương đối trục x của xe tại thời điểm i
     y_i = y_old + midpoint * math.sin(angle_i)         # Vị trí tương đối trục y của xe tại thời điểm i
     print ("x: %f" % x_i, "y: %f" % y_i, "angle: %f" % angle_i)
+    x_old, y_old, angle_old = x_i, y_i, angle_i
 
 while True:
-    Position(0,0,0)
-    control = input("Control DC Motor: ")
-    if control == "0":
+    Position(0, 0, 0)
+    Control = input("Control DC Motor: ")
+    if Control == "0":
         Stopmotor()
-    elif control == "1":
+    elif Control == "1":
         Forward()
-    elif control == "2":
+    elif Control == "2":
         Backward()
-    elif control == "3":
+    elif Control == "3":
         Left()
-    elif control == "4":
+    elif Control == "4":
         Right()
     else:
         PWM.stop(PWMA)
@@ -151,7 +151,4 @@ while True:
         PWM.cleanup()
         myEncoderA.zero()   # Đặt lại vị trí encoderA về 0
         myEncoderB.zero()   # Đặt lại vị trí encoderB về 0
-    Encoder_Wheel()
 
-
-    
--- workspace/position.py
+++ workspace/position.py
@@ -1,13 +1,12 @@
 import math
-
-# Khởi tạo cho trước quãng đường đi được của 2 bánh xe
-wheel_A = 60
-wheel_B = 80
+import random
+import time
 
 def position(x_old, y_old, angle_old):
+    # Khởi tạo cho trước quãng đường đi được của 2 bánh xe
+    wheel_A = random.randint(1, 100)
+    wheel_B = random.randint(1, 100)
     b = 30  # Khoảng cánh giữa 2 bánh xe
-    global wheel_A
-    global wheel_B
     
     midpoint = (wheel_A + wheel_B) / 2        # Độ dịch chuyển tương đối của điểm trung tâm
     angle_new = (wheel_A + wheel_B) / b         # Góc xoay của xe
@@ -17,6 +16,9 @@ def position(x_old, y_old, angle_old):
     x_i = x_old + midpoint * math.cos(angle_i)          # Vị trí tương đối trục x của xe tại thời điểm i
     y_i = y_old + midpoint * math.sin(angle_i)         # Vị trí tương đối trục y của xe tại thời điểm i
     print ("x: %f" % x_i, "y: %f" % y_i, "angle: %f" % angle_i)
+    time.sleep(3)
+    x_old, y_old, angle_old = x_i, y_i, angle_i
 
 while True:
-    position(0, 0, 90)
+
+    position(0, 0, 0)
