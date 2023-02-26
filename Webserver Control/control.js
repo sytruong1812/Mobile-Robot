afcfbe026ed3d8608c00f5e49667ea7ba25a8f11
SY TRUONG <sytruong1812@gmail.com>
2023-02-26 22:24:01 +0700

    Update

---
+++ Webserver Control/control.js
@@ -0,0 +1,16 @@
+var forwardButton = document.getElementById("forward");
+var backwardButton = document.getElementById("backward");
+var leftButton = document.getElementById("left");
+var rightButton = document.getElementById("right");
+
+forwardButton.addEventListener("click", function(){
+    // Thêm hành động di chuyển xe tiến
+});
+
+backwardButton.addEventListener("click", function(){
+    // Thêm hành động di chuyển xe lùi
+});
+
+leftButton.addEventListener("click", function(){
+    // Thêm hành động di chuyển xe sang trái
+});
