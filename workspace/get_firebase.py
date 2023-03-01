1a19354077d4db596c79f15142dc526ef240ae8b
SY TRUONG <sytruong1812@gmail.com>
2023-03-01 09:07:09 +0700

    Update

--- workspace/get_firebase.py
+++ workspace/get_firebase.py
@@ -1,6 +1,9 @@
+import time
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://mobile-robot-6269b-default-rtdb.firebaseio.com/', None)
 result = firebase.get('/Control', None)
 
-print(result)
+while True:
+    result = firebase.get('/Control', None)
+    print(result)
