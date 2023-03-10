7e4bb4d162c47c09e636d36c8a2a81aa1be6cc26
SY TRUONG <sytruong1812@gmail.com>
2023-03-10 10:02:04 +0700

    Update

--- workspace/pid_rr.py
+++ workspace/pid_rr.py
@@ -10,35 +10,43 @@ PWM.start(LPWM, 100, 2000, 1)
 myEncoder = RotaryEncoder(eQEP0)
 myEncoder.setAbsolute()
 
-output = 0; tocdodat = 40; tocdothuc = 0
+tocdodat = 40; tocdothuc = 0
+E = 0; E1 = 0; E2 = 0       # Sai số
+output = 0; last_output = 0     # PWM điều khiển động cơ
 
-def PID_roirac_vantoc(output, tododat, tocdothuc):
-    E = 0; E1 = 0; E2 = 0       # Sai số
+Kp= 0.3; Ki = 0.01; Kd = 0.03
+alpha = 0   
+beta = 0   
+gama = 0    
+T = 0.01       # Thời gian lấy mẫu (Quan trọng)
+PPR = 2390       # Encoder đo được 2390 xung / vòng
 
-    last_output = 0     # PWM điều khiển động cơ
-
-    Kp= 0.3; Ki = 0.01; Kd = 0.03
-    alpha = 0   
-    beta = 0   
-    gama = 0    
-    T = 0.01       # Thời gian lấy mẫu (Quan trọng)
-    PPR = 2390       # Encoder đo được 2390 xung / vòng
-
+def PID_roirac_vantoc():
+    global E, E1, E2, last_output, output
+    global alpha, beta, gama
+    
     pulse = myEncoder.position
-
-    tocdothuc = (pulse / PPR) * (1/T) * 60
+ 
+    tocdothuc = pulse * (PPR/60)
     pulse = 0 
-    # E = tocdodat - tocdothuc
-    if (tocdodat > tocdothuc): E = abs(tocdodat) - abs(tocdothuc)
-    if (tocdodat < tocdothuc): E = abs(tocdothuc) - abs(tocdodat)
 
+    E2 = E1
+    E1 = E
+    
+    # Tính sai số E
+    if (tocdodat > tocdothuc):
+        E = tocdodat - tocdothuc
+    elif (tocdodat < tocdothuc):
+        E = tocdodat - tocdothuc
+    else:
+        E = 0
+        
     alpha = (2 * T * Kp) + (Ki * T * T) + (2 * Kd)
     beta = (T * T * Ki) - (4 * Kd) - (2 * T * Kp)
     gama = 2 * Kd
     output = (alpha*E + beta*E1 + gama*E2 + 2*T*last_output) / (2*T)
     last_output = output
-    E2 = E1
-    E1 = E
+    
     if(output > 50):
         output = 50
     if(output < 0):
@@ -53,5 +61,4 @@ def PID_roirac_vantoc(output, tododat, tocdothuc):
     print(output)
 
 while True:
-    PID_roirac_vantoc(output, tocdodat, tocdothuc)
-    
+    PID_roirac_vantoc()
