974b9e2fe54b65d6a6544454b78d71a3c4fa4e48
SY TRUONG <sytruong1812@gmail.com>
2023-04-09 14:53:04 +0700

    Update

--- workspace/uart.py
+++ workspace/uart.py
@@ -1,14 +1,21 @@
-import Adafruit_BBIO.UART as UART
 import serial
 
-UART.setup("UART1")
+'''
+dataSen = 5byte
+ - Quality
+ - Angle
+ - Distance
+'''
+stopPacket = {'A5', '25'}; 		#Stop request
+resetPacket = {'A5', '40'};		#Reset request
 
-ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
+ser = serial.Serial(port = "/dev/ttyO1", baudrate = 115200)		# UART1: /dev/ttyO1, Rx: P9_26, Tx: P9_24
 ser.close()
 ser.open()
 if ser.isOpen():
 	print("Serial is open!")
-ser.write("Hello World!")
+ser.write(stopPacket)
+ser.read(size = 1)		#1byte = 8bit (Start flag|Quality|Angle|Distance|Checksum)
 ser.close()
 
 # Eventually, you'll want to clean up, but leave this commented for now, 
